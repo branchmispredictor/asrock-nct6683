60ff3e8f90156148c96cafd27a14204f76bb8e44
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2025-05-22 10:19:02 -0400

    Add support for ASRock B650I and A620I (#3)
    * Add support for ASRock B650I and A620I
    
    * Update README with supported boards
    
    * Remove redundant customer ID for B650I/A620I

--- nct6683.c
+++ nct6683.c
@@ -235,6 +235,10 @@ struct customer_family_matcher {
 static const struct customer_family_matcher customer_family_table[] = {
 	CUSTOMER_MATCHES_DMI_BOARD("ASRock", "B550 Taichi Razer Edition",
 				   family_asrock_writable_pwm),
+	CUSTOMER_MATCHES_DMI_BOARD("ASRock", "B650I Lightning WiFi",
+				   family_asrock_writable_pwm),
+	CUSTOMER_MATCHES_DMI_BOARD("ASRock", "A620I Lightning WiFi",
+				   family_asrock_writable_pwm),
 	CUSTOMER_MATCHES_CUSTOMER_ID(NCT6683_CUSTOMER_ID_INTEL,
 				     family_intel_generic),
 	CUSTOMER_MATCHES_CUSTOMER_ID(NCT6683_CUSTOMER_ID_MITAC,
@@ -1001,10 +1005,10 @@ store_pwm(struct device *dev, struct device_attribute *attr, const char *buf,
 	default:
 		usleep_range(1000, 2000);
 	}
-	
+
 	nct6683_write(data, NCT6683_REG_PWM_WRITE(index), val);
 	nct6683_write(data, NCT6683_REG_FAN_CFG_CTRL, NCT6683_FAN_CFG_DONE);
-	
+
 	if (pwm_set_delay) {
 		usleep_range(pwm_set_delay, pwm_set_delay + 1000);
 	}
@@ -1379,7 +1383,7 @@ static int nct6683_probe(struct platform_device *pdev)
 	/* By default only instantiate driver if the customer ID is known */
 	if (data->customer_family == family_unknown && !force)
 		return -ENODEV;
-	
+
 	nct6683_init_device(data);
 	nct6683_setup_fans(data);
 	nct6683_setup_sensors(data);
